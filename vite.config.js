2070d9d567612a3d94c3058d2ea85c6bf8aa4663
AlinaLokkinen <bhd524@myy.haaga-helia.fi>
2026-02-17 15:16:21 +0200

    .

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'coffeelisting/',
+  base: '/coffeelisting/',
   plugins: [react(), tailwindcss()],
  
 });
